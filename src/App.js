233e2ab56afcad778de6a559e86dd448dff2f419
Jatin Gambhir <jatin.gambhir@jungleworks.com>
2023-09-13 00:46:33 +0530

    Created Random List User App with delete Functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default App;
+import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export default function App() {
+
+  const [users, setUsers] = useState([]);
+
+  async function getUsers() {
+    const base_url = "https://randomuser.me/api/?results=20";
+
+    const response = await fetch(base_url);
+    const data = await response.json();
+    return data.results ? data.results : []
+  }
+
+  useEffect(() => {
+    getUsers().then((data) => {
+      setUsers(data);
+    }).catch((err) => {
+      console.log(err);
+    });;
+  }, []);
+
+  function deleteUser(deleteIndex) {
+    const filteredUsers = users.filter((user, index) => {
+      return index != deleteIndex
+    });
+
+    setUsers(filteredUsers)
+  }
+
+  return (
+    <Container className='mt-2'>
+      {users && users.map((user, index) => {
+        return (
+          <Card style={{ display: "flex", flexDirection: "row", margin: "0 auto", padding: "1rem" }} key={index}>
+            <Card.Img src={user?.picture?.medium} style={{ width: "8rem", height: "8rem", borderRadius: "50%" }} />
+            <Card.Body style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+              <Card.Title>{user?.name?.title} {user?.name?.first} {user?.name?.last}</Card.Title>
+              <Button style={{ marginLeft: "auto" }} variant="danger" onClick={() => deleteUser(index)}>Delete</Button>
+            </Card.Body>
+          </Card>
+        )
+      })}
+    </Container>
+  )
+}
